5a340b701df4ebf8287ba0f1d99fd1ec85382229
Aiden <ayasharian1@hwemail.com>
2025-09-15 19:34:20 -0700

    Completed activity 2

---
+++ FileHandlingActivity.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileHandlingActivity {
+    public static void main(String[] args) throws IOException {
+        // Your code here
+        
+        // a. Create main directory
+        File dirMain = new File("JavaFileSystem");
+        dirMain.mkdir();
+        // b. Create three text files
+        File notes = new File("JavaFileSystem/notes.txt");
+        notes.createNewFile();
+        File data = new File("JavaFileSystem/data.txt");
+        data.createNewFile();
+        File log = new File("JavaFileSystem/log.txt");
+        log.createNewFile();
+        // c. Write messages to files
+        Files.writeString(Path.of("JavaFileSystem/notes.txt"), "Aiden");
+        Files.writeString(Path.of("JavaFileSystem/data.txt"), "Yasharian");
+        Files.writeString(Path.of("JavaFileSystem/log.txt"), "AY");
+        // d. Read and display file contents
+        String contents = Files.readString(Path.of("JavaFileSystem/notes.txt"));
+        String all = contents;
+        System.out.println(contents);
+        contents = Files.readString(Path.of("JavaFileSystem/data.txt"));
+        all = all.concat("\n" + contents);
+        System.out.println(contents);
+        contents = Files.readString(Path.of("JavaFileSystem/log.txt"));
+        all = all.concat("\n" + contents);
+        System.out.println(contents);
+        // e. Create backup directory
+        File dirBackUp = new File("JavaFileSystem/Backup");
+        dirBackUp.mkdir();
+        // f. Copy contents to backup file
+        File backup = new File("JavaFileSystem/Backup/backup.txt");
+        backup.createNewFile();
+        Files.writeString(Path.of("JavaFileSystem/Backup/backup.txt"), all);
+        // g. List all files in both directories
+        System.out.println("notes.txt\ndata.txt\nlog.txt\nbackup.txt");
+    }
+}
